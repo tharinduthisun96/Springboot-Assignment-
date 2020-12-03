50e72a5a12b4502a07920b4c492ea0302483814a
Tharindu Akalaka <tharindu.thisun123@gmail.com>
2020-12-03 10:58:34 +0530

    adding controller

---
+++ src/main/java/com/springboot/assignment/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.springboot.assignment.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+    public String index(){
+
+        return "Home";
+
+    }
+
+}
